ae577bba65f860bfefe0826190753e5f0af54bd8
Zhosas <lundholm00@hotmail.com>
2022-06-06 10:02:42 +0200

    Fjernelse af java

--- script.js
+++ script.js
@@ -1,12 +1,2 @@
 // JavaScript Document
-/*eslint-env es6*/
 
-const tilTop = document.querySelector(".top");
-
-window.addEventListener("scroll", () => {
-  if (window.pageYOffset > 100) {
-    tilTop.classList.add("active");
-  } else {
-    tilTop.classList.remove("active");
-  }
-})
