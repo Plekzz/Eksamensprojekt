357e310198034f3cc37a89bfbfa4addad455a439
Zhosas <lundholm00@hotmail.com>
2022-06-07 13:38:14 +0200

    Java

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 // JavaScript Document
 /* eslint-env es6 */ 
 
+function hello(){
+
+document.getElementById("tid").innerHTML = change;
+}
+
+var hour=new Date().getHours();
+var change ="";
+
+if (hour >= 9 && hour < 16) {
+    change = "Åben";
+    document.getElementById("tid").style.color="green";
+} else {
+    change = "Lukket";
+    document.getElementById("tid").style.color="red";
+}
