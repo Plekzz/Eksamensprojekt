46f208e12a8f23203e7b861cee567538158131ee
Zhosas <lundholm00@hotmail.com>
2022-06-08 13:46:37 +0200

    Footer og andre små ændringer

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ document.getElementById("tid").innerHTML = change;
 var hour=new Date().getHours();
 var change ="";
 
-if (hour >= 9 && hour < 16) {
+if (hour >= 8 && hour < 16) {
     change = "Åben";
     document.getElementById("tid").style.color = "#44c44a";
 } else {
